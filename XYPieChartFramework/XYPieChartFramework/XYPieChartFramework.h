df95458b261c78060e7df4f2cb26fc09484bf9ae
Colin Reisterer <creisterer@projectdecibel.com>
2015-04-13 12:14:08 -0400

    Add Dynamic Framework with shared scheme
    For Carthage support

---
+++ XYPieChartFramework/XYPieChartFramework/XYPieChartFramework.h
@@ -0,0 +1,19 @@
+//
+//  XYPieChartFramework.h
+//  XYPieChartFramework
+//
+//  Created by Colin Reisterer on 4/13/15.
+//  Copyright (c) 2015 XY Studio. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for XYPieChartFramework.
+FOUNDATION_EXPORT double XYPieChartFrameworkVersionNumber;
+
+//! Project version string for XYPieChartFramework.
+FOUNDATION_EXPORT const unsigned char XYPieChartFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <XYPieChartFramework/PublicHeader.h>
+
+
